2f18fdab5640706f7e66d4ffe7a9e200dc0ee16d
developer <developer@developer.com>
2021-10-09 22:31:29 +1100

    adding basic form validations

--- src/app/feature/capital-balance-calculator/components/input-form/models/create-input-form.ts
+++ src/app/feature/capital-balance-calculator/components/input-form/models/create-input-form.ts
@@ -3,12 +3,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export function getCapitalInputForm(fb: FormBuilder): FormGroup {
   return fb.group({
     salary: [null, Validators.required],
-    contributions: [],
-    inflation_rate: [],
-    earnings: [],
-    fees: [],
-    tax: [],
-    withdrawals: [],
-    withdrawalStartYear: [],
+    contributions: [null, Validators.required],
+    inflation_rate: [null, Validators.required],
+    earnings: [null, Validators.required],
+    fees: [null, Validators.required],
+    tax: [null, Validators.required],
+    withdrawals: [null, Validators.required],
+    withdrawalStartYear: [
+      null,
+      Validators.compose([Validators.required, Validators.pattern('\\d{4}')]),
+    ],
   });
 }
